60b28be9fb3fd3dd72a2a725cb0d0741bc9636d0
Seongmin Son <dannysmson@naver.com>
2022-07-24 20:08:02 +0900

    BOJ_11279 first commit, 최대힙

---
+++ BOJ_11279.py
@@ -0,0 +1,11 @@
+# 최대힙 (#11279)
+import sys
+import heapq
+N = int(sys.stdin.readline())
+heap = list()
+for i in range(N):
+    num = int(sys.stdin.readline())
+    if num == 0: 
+        if len(heap) == 0: print(0)
+        else: print(-heapq.heappop(heap))
+    else: heapq.heappush(heap, -num)
